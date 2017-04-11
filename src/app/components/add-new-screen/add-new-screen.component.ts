1d9ac468f4ae0bd6a82d1460fe22233e3da859a8
Craig Berry <craigb@cccs.co.uk>
2017-04-11 11:06:42 +0100

    Added: SwimLanes, View Tasks, View User, View Project, Add New Project

--- src/app/components/add-new-screen/add-new-screen.component.ts
+++ src/app/components/add-new-screen/add-new-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {cardItem} from '../../Models/card-item.model';
-import {dataStore} from '../../service-layer/datastore.service'
+import {dataStore} from '../../service-layer/datastore.service';
+import {dbAndDataMethods} from '../../helper-functions/database-and-id-methods.method'
 
 @Component({
   selector: 'app-add-new-screen',
@@ -20,8 +21,19 @@ export class AddNewScreenComponent implements OnInit {
   public tearDown(){
     AddNewScreenComponent.visible = false;
   }
-  public log(_any:any){
-
+  public addNewItem(_ds){
+    var _addNew: cardItem = new cardItem(
+      dbAndDataMethods.getId(_ds.cardList),
+      this.name,
+      3, 
+      1,
+      "Awaiting Approval"
+      );
+      _ds.cardList.push(_addNew);
+      //When DB is connected up then change the below to 
+      //tear down the screen after the network call has returned
+      //successfully
+      this.tearDown();
   }
   constructor() {
     
---
+++ src/app/helper-functions/database-and-id-methods.method.ts
@@ -0,0 +1,16 @@
+import {cardItem} from '../Models/card-item.model'
+
+export class dbAndDataMethods{
+    public static sortByKey(array, key) {
+        return array.sort(function(a, b) {
+            var x = a[key]; var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    }
+    public static getId(_array: Array<cardItem>): number{
+        console.log(_array)
+        var _sortedArray = dbAndDataMethods.sortByKey(_array, "project")
+        var idPlusOne: number = _sortedArray[0]["project"] + 1
+        return idPlusOne;
+    }
+}
